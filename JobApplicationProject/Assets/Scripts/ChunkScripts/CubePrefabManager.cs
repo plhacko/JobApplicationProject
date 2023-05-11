0dce4f24e49001260184b87b47e3f2e121b21b8c
Plhák Tomáš <plhacko@gmail.com>
2023-05-11 23:49:25 +0200

    mining Time added

--- JobApplicationProject/Assets/Scripts/ChunkScripts/CubePrefabManager.cs
+++ JobApplicationProject/Assets/Scripts/ChunkScripts/CubePrefabManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,31 @@ using UnityEngine;
 public class CubePrefabManager : Singleton<CubePrefabManager>
 {
     [SerializeField] GameObject ErrorCubePrefab;
-    [Tooltip("Coresponds to the CubeEnums")]
-    [SerializeField] GameObject[] CubePrefabs;
+    [SerializeField] float DefaultMiningTime = 10f;
 
+    [Tooltip("Coresponds to the CubeEnums")]
+    [SerializeField] CubePrefabTupplle[] CubePrefabs;
     public GameObject GetCubePrefab(CubeEnum c)
     {
         if (CubePrefabs.Length > (int)c)
-            return CubePrefabs[(int)c];
+            return CubePrefabs[(int)c].Prefab;
         else
             return ErrorCubePrefab;
     }
+
+    public float GetRequiredMiningTime(CubeEnum c)
+    {
+        if (CubePrefabs.Length > (int)c)
+            return CubePrefabs[(int)c].MineTime;
+        else
+            return DefaultMiningTime;
+    }
+
+}
+[Serializable]
+struct CubePrefabTupplle
+{
+    public GameObject Prefab;
+    public float MineTime;    
 }
 
--- JobApplicationProject/Assets/Scripts/PlayerScripts/Player.cs
+++ JobApplicationProject/Assets/Scripts/PlayerScripts/Player.cs
@@ -2,67 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Player : MonoBehaviour
 {
     MouseLook MouseLook;
     CubeEnum BuildingMaterial = CubeEnum.snow;
 
+    float CurrentMiningTime = 0f;
+    Vector3Int CurrentMiningTarget;
+    float RequiredMiningTime;
+
+    Transform GUICursor;
+
     void Start()
     {
+        GUICursor = GameObject.Find("Cursor").transform;
         MouseLook = GetComponentInChildren<MouseLook>();
     }
 
     void Update()
     {
         // destroy cubes
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hitObject = MouseLook.GetRaycastHit();
-            if (hitObject.transform != null)
-            {
-                Vector3Int hitObjectVector = new Vector3Int(
-                    (int)hitObject.transform.position.x,
-                    (int)hitObject.transform.position.y,
-                    (int)hitObject.transform.position.z);
+        if (Input.GetMouseButton(0))
+        { MineCubeAtRaycast(); }
+        else { CurrentMiningTime = 0f; }
 
-                ChunkManager.Instance.SetCubeAt(hitObjectVector, CubeEnum.empty);
-            }
-        }
-        //change build cubes
-        if(Input.GetKeyDown(KeyCode.Alpha1)) { BuildingMaterial = CubeEnum.rock; }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) { BuildingMaterial = CubeEnum.grass; }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) { BuildingMaterial = CubeEnum.snow; }
-        // TODO: rm : debug
-        GameObject.Find("BuildingBlockText").GetComponent<TextMeshProUGUI>().text = BuildingMaterial.ToString();
-        
+        // gives some feedback to the player
+        GUICursor.localScale = Vector3.one * (1 + CurrentMiningTime);
+
+        // chcecks if player hasents changed the cube to build with
+        PickBuildingCube();
+
         // build cuubes
         if (Input.GetMouseButtonDown(1))
+        { PlaceCubeAtRaycast(); }
+    }
+
+    private void PlaceCubeAtRaycast()
+    {
+        RaycastHit hitObject = MouseLook.GetRaycastHit();
+
+        if (hitObject.transform != null)
         {
-            RaycastHit hitObject = MouseLook.GetRaycastHit();
+            // gets directional vector from the middle of the cube to the point whre the ray hitted it
+            var dirVec = hitObject.point - hitObject.transform.position;
 
-            if (hitObject.transform != null)
-            {
-                // gets directional vector from the middle of the cube to the point whre the ray hitted it
-                var dirVec = hitObject.point - hitObject.transform.position;
+            // isolates the biggest number from this directional vector and normalizes it
+            float max = Mathf.Max(Mathf.Abs(dirVec.x), Mathf.Abs(dirVec.y), Mathf.Abs(dirVec.z));
+            dirVec.x = Mathf.Abs(dirVec.x) >= max ? dirVec.x : 0;
+            dirVec.y = Mathf.Abs(dirVec.y) >= max ? dirVec.y : 0;
+            dirVec.z = Mathf.Abs(dirVec.z) >= max ? dirVec.z : 0;
+            dirVec.Normalize();
 
-                // isolates the biggest number from this directional vector and normalizes it
-                float max = Mathf.Max(Mathf.Abs(dirVec.x), Mathf.Abs(dirVec.y), Mathf.Abs(dirVec.z));
-                dirVec.x = Mathf.Abs(dirVec.x) >= max ? dirVec.x : 0;
-                dirVec.y = Mathf.Abs(dirVec.y) >= max ? dirVec.y : 0;
-                dirVec.z = Mathf.Abs(dirVec.z) >= max ? dirVec.z : 0;
-                dirVec.Normalize();
+            // gets position of the neighbour cube with the same face
+            Vector3Int hitObjectVector = new Vector3Int(
+                (int)(hitObject.transform.position.x + dirVec.x),
+                (int)(hitObject.transform.position.y + dirVec.y),
+                (int)(hitObject.transform.position.z + dirVec.z));
 
-                // gets position of the neighbour cube with the same face
-                Vector3Int hitObjectVector = new Vector3Int(
-                    (int)(hitObject.transform.position.x + dirVec.x),
-                    (int)(hitObject.transform.position.y + dirVec.y),
-                    (int)(hitObject.transform.position.z + dirVec.z));
-
-                // instantiates the cube
-                if (ChunkManager.Instance.GetCubeTypeAt(hitObjectVector) == CubeEnum.empty)
-                    ChunkManager.Instance.SetCubeAt(hitObjectVector, BuildingMaterial);
-            }
+            // instantiates the cube
+            if (ChunkManager.Instance.GetCubeTypeAt(hitObjectVector) == CubeEnum.empty)
+                ChunkManager.Instance.SetCubeAt(hitObjectVector, BuildingMaterial);
         }
+    }
+
+    private void PickBuildingCube()
+    {
+        //change build cubes
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { BuildingMaterial = CubeEnum.rock; }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { BuildingMaterial = CubeEnum.grass; }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { BuildingMaterial = CubeEnum.snow; }
+
+        // TODO: rm : debug
+        GameObject.Find("BuildingBlockText").GetComponent<TextMeshProUGUI>().text = $"building block: {BuildingMaterial}";
+    }
+
+    private void MineCubeAtRaycast()
+    {
+        RaycastHit hitObject = MouseLook.GetRaycastHit();
+        if (hitObject.transform == null) return;
+
+        Vector3Int newMiningTarget = new Vector3Int(
+            (int)hitObject.transform.position.x,
+            (int)hitObject.transform.position.y,
+            (int)hitObject.transform.position.z);
+
+        // chech for switching targets
+        if (CurrentMiningTarget != newMiningTarget)
+        {
+            CurrentMiningTarget = newMiningTarget;
+            CubeEnum newMiningTargetType = ChunkManager.Instance.GetCubeTypeAt(CurrentMiningTarget);
+            RequiredMiningTime = CubePrefabManager.Instance.GetRequiredMiningTime(newMiningTargetType);
+            CurrentMiningTime = 0f;
+        }
+        else
+        { CurrentMiningTime += Time.deltaTime; }
+
+        // mine (destroy) a cube
+        if (RequiredMiningTime <= CurrentMiningTime)
+        { ChunkManager.Instance.SetCubeAt(CurrentMiningTarget, CubeEnum.empty); }
     }
 }
